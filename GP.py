2508f1f33de2b324897e13f0cf19fcafbd961537
bapimentel <38667475+bapimentel@users.noreply.github.com>
2018-04-23 20:49:48 -0300

    Add files via upload

---
+++ GP.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 23 17:37:04 2018
+
+@author: Bruno Pimentel
+"""
+
+
+
+class GPClass(object):
+    def __init__(self):
+        pass
+        
+    def run_parallel(self,parameters, fit_function, size_final_population):
+        #Executa o metodo GP
+        #Retorna 
+        return 0
