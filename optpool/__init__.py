a2936365644c2df2eba7e64fb3c2d03aabfb34ec
bapimentel <38667475+bapimentel@users.noreply.github.com>
2018-04-30 12:52:04 -0300

    Update __init__.py

--- optpool/__init__.py
+++ optpool/__init__.py
@@ -2,3 +2,4 @@ from optpool.optpool import OptPool
 from optpool.annealingglass import AnnealingGlass
 from optpool.optimizer import Optimizer
 from optpool.resultopt import ResultOpt
+from optpool.PSO import PSO
