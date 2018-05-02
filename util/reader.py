e5f306bb89d80e91e705d9c28ff3643191d5ec07
bapimentel <38667475+bapimentel@users.noreply.github.com>
2018-05-02 16:17:36 -0300

    Create reader.py

---
+++ util/reader.py
@@ -0,0 +1,32 @@
+"""
+File.....: reader.py
+Author...: Bruno Pimentel
+Email....: bappimentel@gmail.com
+Github...: https://github.com/bapimentel
+Description:
+"""
+
+import csv
+import numpy
+
+class Reader():
+    def __init__(self):
+      self.type = "reader"
+      
+    def read_file(self, file_name):
+        reader = csv.reader(open(file_name, "r"), delimiter="\t")
+        x = list(reader)
+        result = numpy.array(x)
+        return result
+    
+    def get_max_min_comp(self, file_name):
+        # file_name = "AtomFrequency.csv"
+        result = self.read_file(file_name)
+        selected_columns = [3,4]
+        max_min_comp = []
+        result = [result[i] for i in range(1,len(result))]
+        for row in result:
+            maximum = float(row[selected_columns[0]])
+            minimum = float(row[selected_columns[1]])
+            max_min_comp.append((maximum,minimum))
+        return max_min_comp
