1bcff1625636e392f2f8f404d949c128bf7fa91e
Bruno Pimentel <bappimentel@gmail.com>
2018-08-09 15:22:49 -0300

    app

--- gg_gui/src/app.py
+++ gg_gui/src/app.py
@@ -34,7 +34,7 @@ class App(QMainWindow, Ui_main_window):
         self.setupUi(self)
 
         #Train RF
-        self.usa_RF = True
+        self.usa_RF = False
         self.model_rf = None
         if self.usa_RF:
             self.init_clf()
